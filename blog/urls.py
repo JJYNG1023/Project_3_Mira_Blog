5e49157c87861ea701ebcf827eecacbdf585c077
JJYNG1023 <j.yeung2020@outlook.com>
2026-05-20 21:56:34 +0000

    outline of nav-bar

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,26 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('blog/', views.home, name='blog'), 
     # This line is added to handle the /blog/ URL and render the same home view
 
-    path('blog/', views.post_list, name='post_detail'),
+   # path('blog/', views.home, name='blog'), 
+    # This line is added to handle the /blog/ URL and render the same home view
+
+   # path('blog/', views.post_list, name='post_detail'),
     # This displays the list of blog posts at the /blog/ URL
 
-    path('blog/<slug:slug>/', views.post_detail, name='new_post'),
+   # path('blog/<slug:slug>/', views.post_detail, name='new_post'),
     # This line is individual post detail view, which will display the details of a specific blog post based on its slug
+
+   # path('search/', views.search, name='search'),
+    # This line is added to handle the search functionality at the /search/ URL
+
+   # path('create_post/', views.create_post, name='create_post'),
+    # This line is added to handle the creation of new blog posts at the /create_post/ URL
+
+   # path('profile/', views.profile, name='profile'),
+    # This line is added to handle the user profile view at the /profile/ URL
+
+   # path('notifications/', views.notifications, name='notifications'),
+    # This line is added to handle the notifications view at the /notifications/ URL
 ]
