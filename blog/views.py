ee61a18fb32af6b5d31a6a37bdb801ed25571cd4
JJYNG1023 <j.yeung2020@outlook.com>
2026-05-22 18:02:59 +0000

    Create dynamic posts and tags on landing page

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Post, Tag
 
 # Create your views here.
 def home(request):
-    return render(request, 'blog/index.html')
+    posts= Post.objects.filter(is_published=True)
+    tags = Tag.objects.all()
+    context = {
+        'posts': posts,
+        'tags': tags,
+    }
+
+    return render(request, 'blog/index.html', context)
     
