ea836eb49b43f901e5d4294188547d6d47bcb762
JJYNG1023 <j.yeung2020@outlook.com>
2026-05-25 13:12:13 +0000

    fix indentation of post_detail model in views.py

--- blog/views.py
+++ blog/views.py
@@ -28,7 +28,7 @@ def post_detail(request, slug):
     if edit_comment_id and request.user.is_authenticated:
         edit_comment = get_object_or_404(Comment, id=edit_comment_id, post=post, author=request.user)
 
-    edit_comment_form = CommentForm(instance=edit_comment)
+        edit_comment_form = CommentForm(instance=edit_comment)
 
     # If the request method is not POST, create an empty instance of the CommentForm to be rendered in the template for users to submit new comments.
     # only authenticated users can submit comments, if the form is valid, it creates a new comment instance, associates it with the current post and the authenticated user, and saves it to the database. After saving the comment, it redirects the user back to the post detail page to see their newly added comment.
@@ -37,30 +37,38 @@ def post_detail(request, slug):
             return redirect('account_login')
         
         comment_id = request.POST.get('comment_id')
+        delete_comment_id = request.POST.get('delete_comment_id')
+   
+    #delete exiting comment       
+        if delete_comment_id:
+            comment = get_object_or_404(Comment, id=delete_comment_id, post=post, author=request.user)
+            comment.delete()
+            messages.success(request, 'Comment has been deleted')
+            return redirect(f"{reverse('post_detail', kwargs={'slug': post.slug})}#comments")
 
     #update/edit exiting comment       
-        if comment_id : 
+        elif comment_id : 
             comment = get_object_or_404(Comment, id=comment_id, post=post, author=request.user)
         
-        comment_form = CommentForm(data=request.POST, instance=comment)
+            comment_form = CommentForm(data=request.POST, instance=comment)
 
-        if comment_form.is_valid():
-            comment_form.save()
-            messages.success(request, 'Comment has been updated')
-            return redirect(f"{reverse('post_detail', kwargs={'slug': post.slug})}#comment-{comment.id}")
+            if comment_form.is_valid():
+                comment_form.save()
+                messages.success(request, 'Comment has been updated')
+                return redirect(f"{reverse('post_detail', kwargs={'slug': post.slug})}#comment-{comment.id}")
 
     #create new comment
         else:
             comment_form = CommentForm(data=request.POST)
     
-        if comment_form.is_valid():
-            comment = comment_form.save(commit=False)
-            comment.post = post
-            comment.author = request.user
-            comment.save()
-            messages.success(request, 'Comment has been posted')
+            if comment_form.is_valid():
+                comment = comment_form.save(commit=False)
+                comment.post = post
+                comment.author = request.user
+                comment.save()
+                messages.success(request, 'Comment has been posted')
 
-            return redirect('post_detail', slug=post.slug)
+                return redirect('post_detail', slug=post.slug)
     else:
         comment_form = CommentForm()
        
