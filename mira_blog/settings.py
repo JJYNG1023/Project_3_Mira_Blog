a23326180e0dfb522332427e764000cfb7fba399
JJYNG1023 <j.yeung2020@outlook.com>
2026-05-31 14:47:10 +0100

    Prepare settings for deployment

--- mira_blog/settings.py
+++ mira_blog/settings.py
@@ -26,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-4#4j)*g_j@ryx8u2=9ri-(4)0d8i850+s!zd74vdvh3fjfxq9v"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1',  "localhost", 'mira-blog-c09c40ce2113.herokuapp.com']
 
