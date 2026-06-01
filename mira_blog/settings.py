d140ff8c7c23a83adb6c45f52ba06d6802c61991
JJYNG1023 <j.yeung2020@outlook.com>
2026-06-01 20:07:26 +0100

    Update production database settings

--- mira_blog/settings.py
+++ mira_blog/settings.py
@@ -124,7 +124,9 @@ if "test" in sys.argv:
     }
 elif "DATABASE_URL" in os.environ:
     DATABASES = {
-        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"))
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL"),
+            conn_max_age=600,
+            ssl_require=True)
     }
 else:
     DATABASES = {
