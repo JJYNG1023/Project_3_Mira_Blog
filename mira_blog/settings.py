2dbc0a83a7e5ecc6211e055938968427e333a068
JJYNG1023 <j.yeung2020@outlook.com>
2026-06-01 20:21:38 +0100

    Allow WhiteNoise to serve static files

--- mira_blog/settings.py
+++ mira_blog/settings.py
@@ -175,6 +175,7 @@ STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+WHITENOISE_USE_FINDERS = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
